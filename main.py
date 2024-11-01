e4cf2c552b9acd00173e8ae456c654202e715837
Dima418 <dziubenko.dima98@gmail.com>
2024-11-01 13:46:02 +0200

    feat(core): add admin endpoints

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from north_admin import setup_admin
 
+from app.core.admin import admin_app
 from app.routers.auth_router import router as user_router
 from app.routers.dev_router import router as dev_router
 from app.routers.healthcheck_router import router as healthcheck_router
@@ -24,3 +26,8 @@ app.include_router(dev_router)
 app.include_router(healthcheck_router)
 app.include_router(order_router)
 app.include_router(statement_router)
+
+setup_admin(
+    admin_app=admin_app,
+    app=app,
+)
