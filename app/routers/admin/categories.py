e77e12bef9c00706926eac138a51c18c14ea1a0c
Dima418 <dziubenko.dima98@gmail.com>
2024-11-08 13:48:26 +0200

    feat: add sorting and filtering for categories

--- app/routers/admin/categories.py
+++ app/routers/admin/categories.py
@@ -1,5 +1,6 @@
+import enum
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy import select, func
+from sqlalchemy import select, func, Enum, String, inspect
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from uuid import UUID
@@ -12,18 +13,56 @@ from app.services.auth_service import get_current_user
 categories_router = APIRouter(tags=["Admin Categories"])
 
 
+column_names = [column.name for column in inspect(Category).columns]
+CategoryColumns = enum.Enum("CategoryColumns", {name: name for name in column_names})
+
+
+class SortOrder(str, enum.Enum):
+    asc = "asc"
+    desc = "desc"
+
+
 @categories_router.get("/categories")
 async def get_categories(
     page: int = 1,
     page_size: int = 100,
+    sort_by: CategoryColumns | None = None,
+    sort_order: SortOrder = SortOrder.asc,
+    filter_by: CategoryColumns | None = None,
+    filter_value: str | None = None,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
+    # set the offset to start from
     offset = (page - 1) * page_size
-    select_query = select(Category).offset(offset).limit(page_size)
-    categories = (await db.execute(select_query)).scalars().all()
-    count_query = select(func.count()).select_from(Category)
+
+    # create a base select query
+    base_query = select(Category)
+    # select_query = select(Category).offset(offset).limit(page_size)
+
+    # if sort_by is passed, sort the query by the column
+    if sort_by:
+        if sort_order == "desc":
+            base_query = base_query.order_by(getattr(Category, sort_by.value).desc())
+        else:
+            base_query = base_query.order_by(getattr(Category, sort_by.value))
+
+    # if filter_by and filter_value are passed, filter the query by the column
+    if filter_by and filter_value:
+        column_attr = getattr(Category, filter_by.value)
+        if isinstance(column_attr.type, Enum):
+            base_query = base_query.filter(column_attr.cast(String).ilike(f"%{filter_value}%"))
+        else:
+            base_query = base_query.filter(column_attr.ilike(f"%{filter_value}%"))
+
+    # get the categories records from the database
+    paginated_query = base_query.offset(offset).limit(page_size)
+    categories = (await db.execute(paginated_query)).scalars().all()
+
+    # get the total count of categories
+    count_query = select(func.count()).select_from(base_query.subquery())
     total = (await db.execute(count_query)).scalar()
+
     return {"items": categories, "total": total}
 
 
